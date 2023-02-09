0d0c96720989bba89ff9a5ca479cfc0c9b861e2c
Marie-Geneste <marie.e.geneste@gmail.com>
2023-02-09 23:17:37 +0100

    Création des différents composant, commencement du Header avec des essais sur le scss

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import './styles.scss';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="navlink">
+        <a href="/">Home</a>
+        <a href="/">Compétences</a>
+        <a href="/">Réalisations</a>
+        <a href="/">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
