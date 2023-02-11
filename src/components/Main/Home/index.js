8f4c60b43ee9579068587d64e51fca4d47e05236
Marie-Geneste <marie.e.geneste@gmail.com>
2023-02-11 19:06:31 +0100

    Composant Home : ajout de style et fonction react pour de l'animation au hover

--- src/components/Main/Home/index.js
+++ src/components/Main/Home/index.js
@@ -1,9 +1,27 @@
+import { useRef } from 'react';
 import './styles.scss';
 
 function Home() {
+  const titleRef = useRef(null);
+
+  function handleMouseOver() {
+    if (titleRef.current) {
+      titleRef.current.style.backgroundPosition = '42% 52%';
+    }
+  }
+
+  function handleMouseOut() {
+    if (titleRef.current) {
+      titleRef.current.style.backgroundPosition = 'initial';
+    }
+  }
+
   return (
-    <div className="main">
-      Marie Geneste
+    <div className="home">
+      <h1 className="title_name" ref={titleRef} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        Marie Geneste
+      </h1>
+      <h1 className="title_job">Développeuse Web</h1>
     </div>
   );
 }
