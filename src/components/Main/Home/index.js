16cef994f00cf3f04af548924e3603fb20157001
Marie-Geneste <marie.e.geneste@gmail.com>
2023-02-12 11:07:21 +0100

    Amélioration de l'animation du title name avec un use effect

--- src/components/Main/Home/index.js
+++ src/components/Main/Home/index.js
@@ -1,24 +1,28 @@
-import { useRef } from 'react';
+import { useEffect } from 'react';
 import './styles.scss';
 
 function Home() {
-  const titleRef = useRef(null);
-
-  function handleMouseOver() {
-    if (titleRef.current) {
-      titleRef.current.style.backgroundPosition = '42% 52%';
-    }
-  }
-
-  function handleMouseOut() {
-    if (titleRef.current) {
-      titleRef.current.style.backgroundPosition = 'initial';
-    }
-  }
+  useEffect(() => {
+    let mouseX;
+    let mouseY;
+    const updateBackgroundPosition = (e) => {
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      const traX = ((4 * mouseX) / 570) + 40;
+      const traY = ((4 * mouseY) / 570) + 50;
+      console.log(traX);
+      document.querySelector('.title_name').style.backgroundPosition = `${traX}% ${traY}%`;
+    };
+    const titleElmt = document.querySelector('.title_name');
+    titleElmt.addEventListener('mousemove', updateBackgroundPosition);
+    return () => {
+      titleElmt.removeEventListener('mousemove', updateBackgroundPosition);
+    };
+  }, []);
 
   return (
     <div className="home">
-      <h1 className="title_name" ref={titleRef} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <h1 className="title_name">
         Marie Geneste
       </h1>
       <h1 className="title_job">Développeuse Web</h1>
