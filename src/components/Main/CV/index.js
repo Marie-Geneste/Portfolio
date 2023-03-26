9dac3cebfd6dac58d13bc52e3b5e76eea3367374
Marie-Geneste <marie.e.geneste@gmail.com>
2023-03-26 23:01:13 +0200

    Finition composant cv

--- src/components/Main/CV/index.js
+++ src/components/Main/CV/index.js
@@ -1,10 +1,15 @@
 import './styles.scss';
+import { GiReturnArrow } from 'react-icons/gi';
 import cv from '../../../assets/C.V.Marie-Geneste.jpg';
 
 function CV() {
   return (
-    <div className="CV" id="CV">
-      <a href="https://smallpdf.com/fr/result#r=8c884eed2c3f3be1e58d4f819e03e285&t=share-document" target="_blank" rel="noreferrer"><img className="cv_image" src={cv} alt="cv" /></a>
+    <div className="cv" id="CV">
+      <h1 className="cv_title">Mon C.V.</h1>
+      <div className="cv_element">
+        <a href="https://smallpdf.com/fr/result#r=8c884eed2c3f3be1e58d4f819e03e285&t=share-document" target="_blank" rel="noreferrer"><img className="cv_image" src={cv} alt="cv" /></a>
+        <span className="cv_element_icon"><GiReturnArrow /></span>
+      </div>
     </div>
   );
 }
