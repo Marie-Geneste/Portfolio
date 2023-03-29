56835d7d6a93e9a26049f342a6512b184c1b5773
Marie-Geneste <marie.e.geneste@gmail.com>
2023-03-29 11:26:45 +0200

    Sauvegarde avant suppression webpack pour gain en performance

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import MediaQuery from 'react-responsive';
 // Styles
 import './styles.scss';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { Link } from 'react-scroll';
 
 function Header() {
   // MOBILE
@@ -22,28 +23,31 @@ function Header() {
   const desktopNav = () => (
     <div className="header">
       <div className="navlink">
-        <a href="/">Home</a>
-        <a href="#competence">Compétences</a>
-        <a href="#realisation">Réalisations</a>
-        <a href="#CV">C.V.</a>
+        <Link activeClass="active" to="home" spy smooth offset={-250} duration={500}>Home</Link>
+        <Link activeClass="active" to="competence" spy smooth offset={-250} duration={500}>Compétences</Link>
+        <Link activeClass="active" to="realisation" spy smooth offset={-175} duration={500}>Réalisations</Link>
+        <Link activeClass="active" to="CV" spy smooth offset={-150} duration={500}>C.V.</Link>
       </div>
     </div>
   );
 
   const mobileNav = () => (
-    <div className="mobile-dropdown">
-      <GiHamburgerMenu
-        className="mobile-dropdown-icon"
-        onClick={handleDropdown}
-      />
+    <div className="header">
+      <div className="mobile-dropdown">
+        <GiHamburgerMenu
+          className="mobile-dropdown-icon"
+          onClick={handleDropdown}
+        />
 
-      <ul className="mobile-nav-links closed" id="mobile-nav-links">
-        <a href="/">Home</a>
-        <a href="#competence">Compétences</a>
-        <a href="#realisation">Réalisations</a>
-        <a href="#CV">C.V.</a>
-      </ul>
+        <ul className="mobile-nav-links closed" id="mobile-nav-links">
+          <Link activeClass="active" to="home" spy smooth offset={-250} duration={500} onClick={handleDropdown}>Home</Link>
+          <Link activeClass="active" to="competence" spy smooth offset={-100} duration={500} onClick={handleDropdown}>Compétences</Link>
+          <Link activeClass="active" to="realisation" spy smooth offset={-100} duration={500} onClick={handleDropdown}>Réalisations</Link>
+          <Link activeClass="active" to="CV" spy smooth offset={-150} duration={500} onClick={handleDropdown}>C.V.</Link>
+        </ul>
+      </div>
     </div>
+
   );
 
   return (
