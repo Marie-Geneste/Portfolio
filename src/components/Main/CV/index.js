96bf6016c2e607c1c353e2fbfbc535b45ce332d4
Marie-Geneste <marie.e.geneste@gmail.com>
2023-03-30 09:54:49 +0200

    Fix pdf

--- src/components/Main/CV/index.js
+++ src/components/Main/CV/index.js
@@ -7,7 +7,7 @@ function CV() {
     <div className="cv" id="CV">
       <h1 className="cv_title">Mon C.V.</h1>
       <div className="cv_element">
-        <a href="https://smallpdf.com/fr/result#r=8c884eed2c3f3be1e58d4f819e03e285&t=share-document" target="_blank" rel="noreferrer"><img className="cv_image" src={cv} alt="cv" /></a>
+        <a href="https://drive.google.com/u/1/uc?id=18ms6Esq-hdhH3xKpUn6eFcQl_mwAXXcV&export=download" target="_blank" rel="noreferrer"><img className="cv_image" src={cv} alt="cv" /></a>
         <span className="cv_element_icon"><GiReturnArrow /></span>
       </div>
     </div>
